3393b7071cc494aa89da71264c7fa144bb94be4b
moguera57 <josephbanwart@gmail.com>
2019-01-22 20:18:04 -0500

    api test

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+const keyTastedive = 'k=328834-ArtistFi-A1N9U9RR&type=music'
+const keyBIT = '?app_id=codingbootcamp'
+const eventsBIT = '/events'
+
+tastedivePull('Pulp')
+bandsInTownPull('Wet Nurse')
+
+function tastedivePull(artist){
+
+    var queryURL = "https://tastedive.com/api/similar?callback=?" + keyTastedive + '&q=' + artist
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    });
+}
+
+function bandsInTownPull(artist){
+    
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + eventsBIT + keyBIT
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    });
+}
